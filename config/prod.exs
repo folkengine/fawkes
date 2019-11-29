08efcb4a2435581db9833a5ae3dd9bd0cbd45a5e
folkengine <gaoler@electronicpanopticon.com>
2019-11-29 14:09:18 -0500

    added giglixir buildpack

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,19 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :fawkes, FawkesWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+       load_from_system_env: true,
+       http: [port: {:system, "PORT"}],
+       server: true,
+       secret_key_base: "${SECRET_KEY_BASE}",
+       url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+       cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :fawkes, Fawkes.Repo,
+       adapter: Ecto.Adapters.Postgres,
+       url: "${DATABASE_URL}",
+       database: "",
+       ssl: true,
+       pool_size: 2
 
 # Do not print debug messages in production
 config :logger, level: :info
